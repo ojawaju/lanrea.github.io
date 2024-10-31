cfb1b1cca7aa92da9df137f6a85627b43da732d9
lanrea <lanreadeniran@gmail.com>
2024-10-30 22:22:40 -0700

    take one of website

--- scripts.js
+++ scripts.js
@@ -1,2 +1,31 @@
-// scripts.js
-
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll(".section");
+    const navLinks = document.querySelectorAll(".nav-item");
+  
+    function changeActiveLink() {
+      let index = sections.length;
+  
+      while (--index && window.scrollY + 70 < sections[index].offsetTop) {}
+      
+      navLinks.forEach((link) => link.classList.remove("active"));
+      navLinks[index].classList.add("active");
+    }
+  
+    changeActiveLink();
+    window.addEventListener("scroll", changeActiveLink);
+  
+    // Smooth scrolling for navigation links
+    navLinks.forEach(link => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        window.scrollTo({
+          top: targetSection.offsetTop - 60, // Offset for header
+          behavior: "smooth"
+        });
+      });
+    });
+  });
